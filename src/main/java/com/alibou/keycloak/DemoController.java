b8061a067a4e6752f4fdf2d82607e2175e49a1c1
ali-bouali <bouali.pro@gmail.com>
2023-05-22 21:43:00 +0200

    create demo controller

---
+++ src/main/java/com/alibou/keycloak/DemoController.java
@@ -0,0 +1,23 @@
+package com.alibou.keycloak;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/demo")
+public class DemoController {
+
+    @GetMapping
+    @PreAuthorize("hasRole('client_user')")
+    public String hello() {
+        return "Hello from Spring boot & Keycloak";
+    }
+
+    @GetMapping("/hello-2")
+    @PreAuthorize("hasRole('client_admin')")
+    public String hello2() {
+        return "Hello from Spring boot & Keycloak - ADMIN";
+    }
+}
