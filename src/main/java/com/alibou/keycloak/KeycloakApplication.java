11b6cddea2b7e8ce0c6de168097d37ad50797b33
ali-bouali <bouali.pro@gmail.com>
2023-05-22 21:41:57 +0200

    init project

---
+++ src/main/java/com/alibou/keycloak/KeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.alibou.keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alibou/keycloak/KeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibou.keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
